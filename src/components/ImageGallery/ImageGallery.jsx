23bce5089c9ab109f1f0eefe5d2ba5eeaa086196
Mariya-Litvin <111592916+Mariya-Litvin@users.noreply.github.com>
2023-04-04 17:07:47 +0300

    update

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -4,6 +4,7 @@ import { Component } from 'react';
 import { resultSearch } from '../api/api';
 import { ImageGalleryEl, ImageGalleryList } from './ImageGallery.styled';
 import { Modal } from '../Modal/Modal';
+
 export class ImageGallery extends Component {
   state = {
     articles: [],
@@ -13,15 +14,23 @@ export class ImageGallery extends Component {
     bigImage: '',
   };
 
-  async componentDidUpdate(prevProps) {
+  async componentDidUpdate(prevProps, prevState) {
     const search = this.props.searchWord.trim();
-    if (prevProps.searchWord !== search && search) {
+    if (
+      (prevProps.searchWord !== search && search) ||
+      prevState.page !== this.state.page
+    ) {
       const page = this.state.page;
       const per_page = this.state.per_page;
+
       try {
         const response = await resultSearch(search, page, per_page);
+        // const totalHits = response.totalHits;
+        // console.log(totalHits);
+        const articles = [...this.state.articles, ...response.hits];
+
         this.setState({
-          articles: response.hits,
+          articles: articles,
         });
       } catch (error) {
         console.log(error.message);
@@ -44,22 +53,48 @@ export class ImageGallery extends Component {
     });
   };
 
+  loadMoreCards = () => {
+    this.setState(prev => ({
+      page: prev.page + 1,
+    }));
+    console.log(this.state.page);
+  };
+
+  onButtonVisible = () => {
+    if (
+      this.state.articles.length < Number(this.state.page * this.state.per_page)
+    ) {
+      return false;
+    }
+    return true;
+  };
+
   render() {
     const { articles } = this.state;
     return (
-      <ImageGalleryList>
-        {articles.map(article => (
-          <ImageGalleryEl key={article.id}>
-            <ImageGalleryItem
-              article={article}
-              handleClick={this.handleBigImg}
+      <>
+        <ImageGalleryList>
+          {articles.map(article => (
+            <ImageGalleryEl key={article.id}>
+              <ImageGalleryItem
+                article={article}
+                handleClick={this.handleBigImg}
+              />
+            </ImageGalleryEl>
+          ))}
+          {this.state.isOpen && (
+            <Modal
+              bigImage={this.state.bigImage}
+              closeModal={this.closeModal}
             />
-          </ImageGalleryEl>
-        ))}
-        {this.state.isOpen && (
-          <Modal bigImage={this.state.bigImage} closeModal={this.closeModal} />
+          )}
+        </ImageGalleryList>
+        {this.onButtonVisible() && (
+          <button type="button" onClick={this.loadMoreCards}>
+            Load more
+          </button>
         )}
-      </ImageGalleryList>
+      </>
     );
   }
 }
